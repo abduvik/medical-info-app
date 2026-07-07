8b079750d8b0c5a8004caf1d32f7d63889868fd3
abduvik <contact@abdu.dev>
2026-07-07 23:28:56 +0200

    Fix Prisma connection

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -5,6 +5,7 @@ import {
   OnModuleInit,
 } from "@nestjs/common";
 import { PrismaClient } from "../../../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 @Injectable()
 export class PrismaService
@@ -12,6 +13,13 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   private readonly logger = new Logger(PrismaService.name);
+
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    super({ adapter });
+  }
 
   async onModuleInit() {
     await this.$connect();
